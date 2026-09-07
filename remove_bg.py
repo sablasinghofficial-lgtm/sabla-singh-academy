65e02a2556e8b563731b88dc3096b471442f0f55
Sabla Singh Academy <sablasinghofficial@gmail.com>
2026-09-07 11:21:49 +0530

    Logo transparent bg removed, brand text black

--- remove_bg.py
+++ remove_bg.py
@@ -1,46 +1,19 @@
 from PIL import Image
 import numpy as np
 
-# Reload original uploaded file
-img = Image.open("logo.png").convert("RGBA")
-data = np.array(img, dtype=np.float64)
+img = Image.open('logo_new.png').convert('RGBA')
+data = np.array(img)
 
-r = data[:,:,0]
-g = data[:,:,1]  
-b = data[:,:,2]
+# Get RGB values
+r, g, b, a = data[:,:,0], data[:,:,1], data[:,:,2], data[:,:,3]
 
-# Navy blue background is approx (10-40, 20-55, 80-130)
-# Use flood-fill style: detect bg color from corners
-# Sample corner pixels to get exact bg color
-corners = [
-    img.getpixel((0, 0)),
-    img.getpixel((10, 10)),
-    img.getpixel((img.width-1, 0)),
-    img.getpixel((0, img.height-1)),
-    img.getpixel((img.width-1, img.height-1)),
-]
-print("Corner colors:", corners)
+# Pixels that are near-black (background) -> make transparent
+# Black background is roughly r<40, g<40, b<40
+black_mask = (r < 50) & (g < 50) & (b < 50)
 
-# Target bg: dark navy blue range
-# Make mask: pixels similar to navy background
-# Navy: R~15-45, G~25-60, B~80-130
-mask = (
-    (r < 65) & (g < 75) & (b > 65) & (b < 145) |  # Navy blue range
-    (r < 20) & (g < 20) & (b < 50)  # Very dark / near black
-)
+# Also remove very dark pixels near edges
+data[black_mask, 3] = 0  # Set alpha to 0 (transparent)
 
-# Apply with smooth edges using alpha feathering
-data_uint8 = np.array(img)
-data_uint8[mask, 3] = 0  # Fully transparent
-
-# Also semi-transparent edge pixels (anti-aliasing)
-# Find edge pixels - neighbors of mask
-from scipy.ndimage import binary_dilation
-dilated = binary_dilation(mask, iterations=2)
-edge_mask = dilated & ~mask
-# Make edge pixels semi-transparent for smooth look
-data_uint8[edge_mask, 3] = (data_uint8[edge_mask, 3] * 0.3).astype(np.uint8)
-
-result = Image.fromarray(data_uint8)
-result.save("logo.png", "PNG")
-print("Transparency done with smooth edges!")
+result = Image.fromarray(data)
+result.save('logo_transparent.png')
+print("Done! Black background removed.")
